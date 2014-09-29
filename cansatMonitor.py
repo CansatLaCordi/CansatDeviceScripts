c241e205d1f65d3adb38d4fb3fcab344c2e9c9bc
adrfrank <guanoman_adrian@hotmail.com>
2014-09-29 18:50:56 -0500

    temporizador en lecturas

--- cansatMonitor.py
+++ cansatMonitor.py
@@ -2,7 +2,9 @@ import Adafruit_BMP.BMP085 as BMP085
 
 Tempsensor = BMP085.BMP085()
 
-print 'Temp = {0:0.2f} *C'.format(Tempsensor.read_temperature())
-print 'Pressure = {0:0.2f} Pa'.format(Tempsensor.read_pressure())
-print 'Altitude = {0:0.2f} m'.format(Tempsensor.read_altitude())
-print 'Sealevel Pressure = {0:0.2f} Pa'.format(Tempsensor.read_sealevel_pressure())
+while True:
+	print 'Temp = {0:0.2f} *C'.format(Tempsensor.read_temperature())
+	print 'Pressure = {0:0.2f} Pa'.format(Tempsensor.read_pressure())
+	print 'Altitude = {0:0.2f} m'.format(Tempsensor.read_altitude())
+	print 'Sealevel Pressure = {0:0.2f} Pa'.format(Tempsensor.read_sealevel_pressure())
+	time.sleep(5)
