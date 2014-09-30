07fa73f4cba3111a2919a417e5e13569c688db8f
adrfrank <adrfrank@outlook.com>
2014-09-30 16:03:25 -0500

    archivo para control de sensores agregado

--- cansatMonitor.py
+++ cansatMonitor.py
@@ -1,13 +1,5 @@
-import time
-import Adafruit_BMP.BMP085 as BMP085
-import os
 
-Tempsensor = BMP085.BMP085()
+import CansatSensorController 
 
-while True:
-	os.system("clear")
-	print 'Temp = {0:0.2f} *C'.format(Tempsensor.read_temperature())
-	print 'Pressure = {0:0.2f} Pa'.format(Tempsensor.read_pressure())
-	print 'Altitude = {0:0.2f} m'.format(Tempsensor.read_altitude())
-	print 'Sealevel Pressure = {0:0.2f} Pa'.format(Tempsensor.read_sealevel_pressure())
-	time.sleep(1)
+cont = CansatSensorController()
+cont.Start()
