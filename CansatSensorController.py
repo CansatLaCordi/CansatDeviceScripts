eed29abf12081144d0f1ddef2647f551b43771a6
adrfrank <adrfrank@outlook.com>
2014-10-05 21:10:48 +0000

    --

--- CansatSensorController.py
+++ CansatSensorController.py
@@ -1,17 +1,17 @@
 import time
-import Adafruit_BMP as BMP
+import Adafruit_BMP.BMP085 as BMP
 import os
 
 class CansatSensorController:
-	_tempSensor = BMP085.BMP085()
+	_tempSensor = BMP.BMP085()
 	_segEspera = 1
 	_active = False
 	def Start(self):
-		_active = True
-		while _active:
-			print 'Temp = {0:0.2f} *C'.format(_tempSensor.read_temperature())
-			print 'Pressure = {0:0.2f} Pa'.format(_tempSensor.read_pressure())
-			print 'Altitude = {0:0.2f} m'.format(_tempSensor.read_altitude())
-			print 'Sealevel Pressure = {0:0.2f} Pa'.format(_tempSensor.read_sealevel_pressure())		
-			time.sleep(_segEspera)	
+		self._active = True
+		while self._active:
+			print 'Temp = {0:0.2f} *C'.format(self._tempSensor.read_temperature())
+			print 'Pressure = {0:0.2f} Pa'.format(self._tempSensor.read_pressure())
+			print 'Altitude = {0:0.2f} m'.format(self._tempSensor.read_altitude())
+			print 'Sealevel Pressure = {0:0.2f} Pa'.format(self._tempSensor.read_sealevel_pressure())		
+			time.sleep(self._segEspera)	
 
